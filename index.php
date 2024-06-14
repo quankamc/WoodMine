8de8d6cabaec7caf6f6878f5b085f4c97b62adc7
WoodMine <quandaikmc@gmail.com>
2024-06-15 01:31:26 +0700

    WoodMine

---
+++ index.php
@@ -0,0 +1,230 @@
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Title -->
+    <title>$WOOD</title>
+
+ 
+
+    <!-- Favicon -->
+    <link rel="icon" href="/images/wood.png" type="image/svg+xml" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/plugins.css" />
+    <link rel="stylesheet" href="/css/style.css" />
+	<link href="https://fonts.cdnfonts.com/css/cascadia-code" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/jetbrains-mono-2" rel="stylesheet">
+                        
+</head>
+
+
+<body style="font-family: 'JetBrains Mono', sans-serif;">
+    <div class="wrapper d-flex flex-column justify-between">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg fixed-top bg-dark" data-bs-theme="dark">
+            <div class="container">
+                <!-- Logo -->
+                <a class="navbar-brand" href="/">
+                    <img src="/images/wood-logo.png" alt="" style="width: 120px;" />
+                </a>
+
+                <!-- Navbar toggler button -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <div class="navbar-toggler-icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+
+                <!-- Navbar content -->
+                <div class="collapse navbar-collapse" id="navbarContent">
+                    <div class="navbar-content-inner ms-lg-auto d-flex flex-column flex-lg-row align-lg-center gap-4 gap-lg-10 p-2 p-lg-0">
+                        <ul class="navbar-nav gap-lg-2 gap-xl-5">
+                            
+                            
+                            <li class="nav-item">
+                                <a class="nav-link " href="/">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#">CHART</a>
+                            </li>
+                           
+                        </ul>
+                        <div class="">
+                            <a href="#" class="btn btn-outline-primary-dark">BUY $WOOD</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+        <main class="flex-grow-1">
+            <!-- Hero -->
+            <section class="hero-section style-1 overflow-hidden bg-dark py-10 py-lg-15" data-bs-theme="dark">
+                <div class="container">
+                    <div class="row justify-center">
+                        <div class="col-lg-9">
+                            <div class="text-center">
+                                <div class="position-relative z-1">
+                                    <p class="text-primary-dark" data-aos="fade-up-sm">
+                                        $WOOD START MINING TO DAY
+                                    </p>
+                                    <h1 class="text-white mb-8" data-aos="fade-up-sm" data-aos-delay="150" style="">
+                                      $WOOD MEMECOIN<br />
+
+                                        <span
+								class="fw-bold text-gradient-2 typed-animation"
+								data-strings='["0% TAX", "MINT REVOKED"]'
+								>LP BURNT</span>
+                                    </h1>
+                                    <a href="#" class="btn btn-lg btn-gradient-1" data-aos="fade-up-sm" data-aos-delay="200">START MINING</a>
+                                </div>
+                                <div data-aos="fade-up-sm" data-aos-delay="300">
+                                    <div class="image-with-shape">
+<!--                                        <img src="assets/images/shapes/blurry-shape-1.svg" alt="" class="shape animate-scale" />-->
+                                        <div class="mt-12 shadow-lg overflow-hidden position-relative z-1">
+                                            <img class="img-fluid d-inline-block" src="/images/wood.png" alt="" style="width:400px;"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <!-- Features -->
+            <section class="py-10 py-lg-15">
+                <div class="container">
+                    <div class="text-center mb-18">
+                        <h1 class="mb-0 text-white" data-aos="fade-up-sm" data-aos-delay="50">
+                           How to Buy $WOOD
+                        </h1>
+                    </div>
+
+                    <div class="row g-6 g-xl-14">
+                        <div class="col-lg-4" data-aos="fade-up-sm" data-aos-delay="200">
+                            <div class="d-flex flex-column gap-6 flex-lg-row">
+                                <div class="icon w-14 h-14 flex-shrink-0 d-flex align-center justify-center rounded-3 p-2 border bg-gradient-3 text-primary-dark border-white border-opacity-10">
+                                   <img src="https://cdn.freelogovectors.net/wp-content/uploads/2023/05/phantom-logo-freelogovectors.net_.png" style="width: 110px;"> 
+                                </div>
+                                <div class="content">
+                                    <h4 class="mb-4 text-white">Create a Wallet</h4>
+                                    <p>
+                                        Download the phantom app or wallet of your choice for free from the app store or Google Play store. Desktop users, download the google chrome extension by visiting phantom.app
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4" data-aos="fade-up-sm" data-aos-delay="250">
+                            <div class="d-flex flex-column gap-6 flex-lg-row">
+                                <div class="icon w-14 h-14 flex-shrink-0 d-flex align-center justify-center rounded-3 p-2 border bg-gradient-3 text-primary-dark border-white border-opacity-10">
+                                    <img src="https://cdn3d.iconscout.com/3d/premium/thumb/solana-10229260-8263859.png" style="width: 100px;">
+                                </div>
+                                <div class="content">
+                                    <h4 class="mb-4 text-white">
+Get Some SOL</h4>
+                                    <p>
+                                       You must have SOL in your wallet to convert to $WOOD. If you don't have SOL, you can buy it directly on the OKX exchange or transfer from another wallet or buy it on another exchange and deposit it to your wallet.
+
+
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4" data-aos="fade-up-sm" data-aos-delay="300">
+                            <div class="d-flex flex-column gap-6 flex-lg-row">
+                                <div class="icon w-14 h-14 flex-shrink-0 d-flex align-center justify-center rounded-3 p-2 border bg-gradient-3 text-primary-dark border-white border-opacity-10">
+                                    <img src="https://thegivingblock.com/wp-content/uploads/2022/04/Raydium-RAY-coin.png" style="width: 100px;">
+                                </div>
+                                <div class="content">
+                                    <h4 class="mb-4 text-white">Connect Raydium Wallet</h4>
+                                    <p>
+                                        Connect to your wallet and paste the $WOOD token address into Raydium, select $WOOD and click confirm. When Phantom prompts you to sign your wallet, click sign
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+            <!-- Features -->
+            <section class="py-10 py-lg-15 @extraClassList">
+                <div class="container">
+                    <div class="row g-6 gx-lg-14 gx-xl-20 align-center">
+                        <div class="col-lg-6" data-aos="fade-up-sm" data-aos-delay="150">
+                            <div class="content">
+                                <p class="text-primary-dark">ABOUT</p>
+                                <center><h1 class="text-white mb-8">
+                                    $WOOD WILL BECOME A POTENTIAL MEMECOIN IN THE FUTURE
+                                </h1></center>
+                                <p class="mb-6">
+                Introducing "$WOOD", a memecoin that simulates the feeling of being a logger and selling them for profit.<br><br> $WOOD has been determined to become a potential memecoin in the future and have an influence on cryptocurrency.
+                                </p>
+                                
+                                
+                            </div>
+                        </div>
+                        <div class="col-lg-6" data-aos="fade-up-sm" data-aos-delay="250">
+                            <div class="feature-img">
+                                <blockquote class="twitter-tweet"><p lang="en" dir="ltr">&mdash; Telegram: <a href="#">updating ...</a></p>&mdash; Twitter/X: <a href="#">updating ...</a></p>&mdash; Email: <a href="mailto:quandaikmc@gmail.com" title="Click to send email!">quandaikmc@gmail.com</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+            </section>
+
+
+            <!-- Use cases -->
+            
+
+            <!-- Reviews -->
+            
+
+
+            <!-- Pricing -->
+            
+
+            <!-- FAQ -->
+            
+
+            <!-- CTA -->
+            
+
+        </main>
+
+        <!-- Footer -->
+        <footer class="footer bg-striped pt-10 pt-lg-15">
+            <div class="container">
+                
+
+                <div class="text-center py-6 mt-8">
+                    <p class="fs-sm mb-0">Copyright <span class="text-primary-dark">$WOOD &copy; 2024</span>. All Rights Reserved.</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+
+
+    <!-- JS -->
+    <script src="/js/plugins.js"></script>
+    <script src="/js/main.js"></script>
+
+</body>
+
+</html>
